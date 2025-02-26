d1e0863feb6f471c09f4115f2a97fc5d79d8c307
scox1502 <samuelc506@deltalearns.ca>
2025-02-26 18:35:14 +0000

    started on it

---
+++ main.py
@@ -0,0 +1,10 @@
+main_guest = []
+amount_of_no = 0 
+while True:
+    amount_of_guests = input("how many people do you want to invite? ")
+    choice = input(f"you want {amount_of_guests} people to go to your party? [y/n] ")
+    if choice == "y":
+        break
+
+    else:
+        pass
