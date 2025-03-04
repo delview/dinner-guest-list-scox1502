4a7df2de5544c6d84be9b67ec195e028ae1b8af0
scox1502 <samuelc506@deltalearns.ca>
2025-03-04 18:59:28 +0000

    fixed the final bug and finished the project :)

--- main.py
+++ main.py
@@ -2,21 +2,31 @@ import random
 
 main_guest = []
 main_invites = []
-amount_of_guests = 7
+amount_of_guests = 8
 new_guest = []
 
 while amount_of_guests > 0:
+    if amount_of_guests > 6:
+        amount_of_guests -= 1
+    
     guest = input("Who would you like to invite? ")
     choice = input(f"Do you want {guest} to be at your party? [y/n] ")
 
     if choice.lower() == "y":
+        if amount_of_guests > 6:
+            amount_of_guests -= 2
+        elif amount_of_guests < 5:
+            amount_of_guests -= 1
+        elif amount_of_guests == 5:
+            amount_of_guests -= 1
         main_guest.append(guest)
-        amount_of_guests -= 1
+
         print(f"You can have {amount_of_guests} more people at the party.")
     elif choice.lower() == "n":
         print("Alright, let's invite someone else.")
     else:
         print("That is not an option. Try again.")
+
         
 print("ok lets make an invitation letter for everyone :)")
 print(f"lets make a letter for {main_guest[0]} first")
@@ -34,12 +44,10 @@ main_invites.append(invites)
 print(f"we cant leave {main_guest [4]} out")
 invites = input(f"dear {main_guest [4]} ")
 main_invites.append(invites)
-print(f"cant forget {main_guest [5]}")
+print(f"and finaliy {main_guest [5]}")
 invites = input(f"dear {main_guest [5]} ")
 main_invites.append(invites)
-print(f"and {main_guest [6]} was saved best for last")
-invites = input(f"dear {main_guest [6]} ")
-main_invites.append(invites)
+
 
 random_index = random.randint(0, len(main_guest) - 1)
 random_person = main_guest[random_index]
@@ -48,7 +56,7 @@ print("we need to invite someone new")
 new_guest = input(f"lets replace {random_person} with ")
 
 random_person = new_guest
-main_guest.remove(f"{random_person}")
+main_guest.remove(f"{main_guest [random_index]}")
 main_guest.append(f"{new_guest}")
 
 print(f"{main_guest}")
